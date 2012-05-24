80e4847b0037a2d95d6ac19786847e5f7795629f
Thorsten Lorenz <thlorenz@gmx.de>
2012-05-23 22:38:25 -0400

    ran express init

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var express = require('express');
+var express = require('express'), 
+    routes = require('./routes');
 
-var app = express.createServer(express.logger());
+var app = module.exports = express.createServer();
 
-app.get('/', function(request, response) {
-  response.send('Welcome to github-doc-toc');
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
